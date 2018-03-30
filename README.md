c4283a77e76b1e4697b29c3701bcf840fb6f97e3
fayeed <fayeed@live.com>
2018-03-30 21:04:32 +0530

    firebase config updated

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Build using Firebase and ReactJS with its new Context API
 
-demo : https://react-to-do-list-app.firebaseapp.com/
+demo : https://react-to-do-list-app-b6425.firebaseapp.com/
--- src/firebase/firebaseconfig.js
+++ src/firebase/firebaseconfig.js
@@ -1,14 +1,14 @@
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
 const config = {
-    apiKey: "AIzaSyCRaiBxXXQgw9dSyZBIobMkSDgG9CyOZKk",
-    authDomain: "react-to-do-list-app.firebaseapp.com",
-    databaseURL: "https://react-to-do-list-app.firebaseio.com",
-    projectId: "react-to-do-list-app",
-    storageBucket: "react-to-do-list-app.appspot.com",
-    messagingSenderId: "934647851460"
-  };
+  apiKey: "AIzaSyDu2A0YMVZxHbLVXpWObsZ26VBS977zF1U",
+  authDomain: "react-to-do-list-app-b6425.firebaseapp.com",
+  databaseURL: "https://react-to-do-list-app-b6425.firebaseio.com",
+  projectId: "react-to-do-list-app-b6425",
+  storageBucket: "",
+  messagingSenderId: "581199972557"
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  export default firebase;
+export default firebase;
