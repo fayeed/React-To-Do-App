7a22e2beb3c28966585feefb98df405d79dc38bb
fayeed <fayeed@live.com>
2018-03-31 14:10:12 +0530

    firebase integration completed and all the CRUD code added

---
+++ src/contexts/MyProvider.js
@@ -0,0 +1,159 @@
+import React, { Component } from "react";
+import * as database from "../firebase/database";
+import * as firebase from 'firebase';
+
+const { Provider, Consumer } = React.createContext();
+
+class MyProvider extends Component {
+  state = {
+    list: [],
+    currentlist: "",
+    id: '',
+    name: "",
+    email: "",
+    password: "",
+    isInputOpen: false,
+    isSignUpOpen: false,
+    isLoggedIn: false
+  };
+
+  chanheID = id => {
+      this.setState({id});
+  }
+
+  changeEmail = email => {
+    this.setState({ email });
+  };
+
+  changeName = name => {
+    this.setState({ name });
+  };
+
+  changePassword = password => {
+    this.setState({ password });
+  };
+
+  toggleInput = () => {
+    this.setState({ isInputOpen: !this.state.isInputOpen });
+  };
+
+  toggleSignUp = () => {
+    this.setState({ isSignUpOpen: !this.state.isSignUpOpen });
+  };
+
+  toggleLoggedIn = () => {
+    if(this.state.isLoggedIn) {
+        this.setState({id: '',email: '', name: '', password: '', isLoggedIn: false})
+    } else {
+        this.setState({isLoggedIn: true})
+    }
+  };
+
+  fetchlist = () => {
+    database.default.readList(this.state.email, todo =>
+      this.setState({ list: todo })
+    );
+  };
+
+  addList = (ele) => {
+    let s = this.state.items ? [...this.state.list] : [];
+    const key = firebase.ref(`/users/${this.state.id}/list/`).push().key;
+    ele.id = key
+    s.push(ele)
+    if(this.state.isLoggedIn) {
+        database.default.pushList(this.state.id, ele)
+    }
+    
+    this.setState({list: s})
+  }
+
+
+  removeList = (id, ele) => {
+    let i = [...this.state.list].filter(ele => ele.id !== id);
+
+    if (this.state.isLoggedIn) {
+      database.default.removeList( this.state.id, id);
+    }
+    
+    this.setState({ list: i });
+  }
+
+  addItem = (id, ele, listID) => {
+    let s = this.state.items ? [...this.state.list] : [];
+    s.forEach((list, index) => {
+        if(list.id === listID) {
+            s[index].items.push(ele) 
+        }
+    }) 
+    this.setState({ list: s });
+
+    if (this.state.isLoggedIn) {
+      database.default.pushToList(this.state.email, ele, id);
+    }
+
+    this.setState({ input: !this.state.input });
+  };
+
+  removeItem = (id, listID) => {
+    let s = this.state.items ? [...this.state.list] : [];
+    s.forEach((list, index) => {
+        if(list.id === listID) {
+            s[index].items = s[index].items.filter((ele, i) => ele) 
+        }
+    }) 
+
+    if (this.state.isLoggedIn) {
+      database.default.removeFromList( this.state.id, id);
+      
+    }
+
+    this.setState({ list: s });
+  };
+
+  getUser = () => {
+    if (!this.state.isLoggedIn && this.state.isSignUpOpen) {
+      database.default.getUserData(
+        this.state.email,
+        this.state.password,
+        (id, name, email, password) => this.setState({ id, name, email, password })
+      );
+    }
+  };
+
+  addUser = () => {
+    if (!this.state.isLoggedIn && this.state.isSignUpOpen) {
+      database.default.writeUserData(
+        this.state.name,
+        this.state.email,
+        this.state.password,
+        (id, name, email, password) => this.setState({ id, name, email, password })
+      );
+    }
+  };
+
+  render() {
+    return (
+      <Provider
+        value={{
+          ...this.state,
+          changeEmail: this.changeEmail,
+          changeName: this.changeName,
+          changePassword: this.changePassword,
+          toggleInput: this.toggleInput,
+          toggleLoggedIn: this.toggleLoggedIn,
+          toggleSignUp: this.toggleSignUp,
+          fetchList: this.fetchlist,
+          addItem: this.addItem,
+          removeItem: this.removeItem,
+          addUser: this.addUser,
+          getUser: this.getUser,
+        }}
+      >
+        {this.props.children}
+      </Provider>
+    );
+  }
+}
+
+export default { MyProvider };
+export { Consumer };
---
+++ src/firebase/database.js
@@ -0,0 +1,144 @@
+import config from "./firebaseconfig";
+import * as firebase from "firebase";
+
+const database = config.database();
+
+const pushToList = (id, ele, listID) => {
+  database
+    .ref(`/users/${id}/list/`)
+    .orderByChild('id')
+    .equalTo(listID)
+    .once("value")
+    .then(snapshot => {
+      snapshot.forEach(childSnapshot => {
+      let todos = childSnapshot.val() || [];
+
+      const d = new Date();
+
+      todos.items.push({
+        id: ele.id,
+        message: ele.message,
+        category: ele.category,
+        completed: false,
+        time: `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`
+      });
+
+      database.ref(`users/${id}/list/`).orderByChild('id')
+      .equalTo(listID).set(todos);
+      console.log("complete");
+    });
+  });
+};
+
+const removeFromList = (id, itemId) => {
+  database
+    .ref(`/users/${id}/list/`)
+    .orderByChild("id")
+    .equalTo(itemId)
+    .once("value")
+    .then(snapshot => {
+      snapshot.forEach(childSnapshot => {
+        database
+          .ref(`/users/${id}/list/`)
+          .orderByChild("id")
+          .equalTo(itemId)
+          .child(childSnapshot.key)
+          .remove();
+      });
+    });
+};
+
+const readList = (id, callback) => {
+  database
+    .ref(`/users/${id}/list/`)
+    .once("value")
+    .then(todo => {
+      callback(todo.val());
+    });
+};
+
+const pushList = (id, ele) => {
+  database
+    .ref(`/users/${id}/list/`)
+    .once("value")
+    .then(todo => {
+
+      const d = new Date();
+      const list = {
+        id: ele.id,
+        name: ele.name,
+        items: [],
+        time: `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`
+      }
+
+      todo.push(list)
+
+      database.ref(`users/${id}/list/`).set(todo);
+      console.log("complete");
+    });
+}
+
+const removeList = (id, itemId) => {
+  database
+    .ref(`/users/${id}/list/`)
+    .orderByChild("id")
+    .equalTo(itemId)
+    .once("value")
+    .then(snapshot => {
+      snapshot.forEach(childSnapshot => {
+        database
+          .ref(`/users/${id}/list/`)
+          .child(childSnapshot.key)
+          .remove();
+      });
+    });
+};
+
+const getUserData = (email, password, callback) => {
+  database
+    .ref("/users/")
+    .orderByChild("email")
+    .equalTo(email)
+    .once("value")
+    .then(snapshot => {
+      snapshot.forEach(childSnapshot => {
+        if (childSnapshot.val() === null) {
+          return false;
+        } else if (
+          childSnapshot.val().email !== email &&
+          childSnapshot.val().password !== password
+        ) {
+          return false;
+        } else {
+          callback(
+            childSnapshot.val().id,
+            childSnapshot.val().name,
+            childSnapshot.val().email,
+            childSnapshot.val().password
+          );
+
+          return true;
+        }
+      });
+    })
+    .catch(e => {
+      console.log(e);
+    });
+};
+
+const writeUserData = (name, email, password, callback) => {
+  
+  const key = firebase.ref('/users/').push().key;
+
+  database
+    .ref("users/" + key)
+    .set({
+      id: key,
+      email,
+      password,
+      name
+    })
+    .then(callback(key, name, email, password));
+};
+
+export default { pushToList, removeFromList, readList, pushList, removeList, getUserData, writeUserData };
